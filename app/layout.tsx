98d548bec4f9a27f961a16b48e3cf0b3831361f6
Nicholas Kim <nicholaskimto@gmail.com>
2026-07-30 14:07:12 -0400

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,6 +59,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           </a>
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   )
